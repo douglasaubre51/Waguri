07989ff8eea7f63fc276b1d45a410728f19e52c9
allen <boseallen192@gmail.com>
2025-08-27 11:09:05 +0530

    finished sign-up

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,11 +4,16 @@ using Api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+// load env variables
+DotNetEnv.Env.Load();
 
 // add connection to waguridb
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -63,15 +68,42 @@ app.MapPost("/sign-up", async (
             Debug.WriteLine($"account for {dto.UserName} couldnot be created!");
             return Results.BadRequest();
         }
-        // success
 
-        return Results.Created();
+        // send confirmation email
+        var token = _userManager.GenerateEmailConfirmationTokenAsync(user);
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+
+        var client = new SendGridClient(apiKey);
+        var from = new EmailAddress("boseallen192@gmail.com", "allen");
+        var subject = "Confirmation Email for Waguri Account Registration";
+        var to = new EmailAddress(user.Email, user.UserName);
+        var plainTextContent = "Lasts only for a day!";
+        var htmlContent = $"<h1>This is your account confirmation token!\n{token}</h1>";
+        var email = MailHelper.CreateSingleEmail(
+            from,
+            to,
+            subject,
+            plainTextContent,
+            htmlContent
+            );
+
+        var status = await client.SendEmailAsync(email);
+
+        return Results.Ok();
     }
     catch (Exception ex)
     {
         Debug.WriteLine($"SignIn error:\n{ex}");
         return Results.InternalServerError();
     }
+});
+
+// /sign-up/confirm
+app.MapPost("/sign-up/confirm", (
+    [FromBody] string token
+    ) =>
+{
+
 });
 
 app.Run();
