b10e1202a4b89782cdf20fded0911794dbcaa1c5
allen <boseallen192@gmail.com>
2025-08-30 15:34:40 +0530

    added confirm user for aira

--- Api/Program.cs
+++ Api/Program.cs
@@ -92,6 +92,64 @@ app.MapGet("/get-all-users",
             return Results.InternalServerError();
         }
     });
+// manually confirm user account
+app.MapGet(
+    "/user/confirm/{userName}",
+    async (
+        string userName,
+        [FromServices] UserManager<User> _userManager
+        ) =>
+    {
+        try
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+            // send confirmation email
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+            var callbackUrl = $"http://localhost:5000/sign-up/confirm/{user.Id}/{code}";
+            var appPassword = Environment.GetEnvironmentVariable("EMAIL_APP_PASSWORD");
+            var senderMail = "douglasaubre@gmail.com";
+            var senderName = "douglas aubre";
+            // craft email
+            var email = new MimeMessage();
+            email.From.Add(
+                new MailboxAddress(senderName, senderMail)
+                );
+            email.To.Add(
+                new MailboxAddress(user.FirstName, user.Email)
+                );
+            email.Subject = "Waguri account confirmation";
+            email.Body = new TextPart("html")
+            {
+                Text = $"<h1>Confirmation code :</h1><strong>{callbackUrl}</strong><br>only lasts for a day!"
+            };
+            // mail client
+            Debug.WriteLine($"sending email to : {user.Email}");
+            using var smtpClient = new SmtpClient();
+            await smtpClient.ConnectAsync(
+                "smtp.gmail.com",
+                587,
+                MailKit.Security.SecureSocketOptions.StartTls
+                );
+            await smtpClient.AuthenticateAsync(
+                senderMail,
+                appPassword
+                );
+            await smtpClient.SendAsync(email);
+            Debug.WriteLine($"email has been sent to : {user.Email}");
+            await smtpClient.DisconnectAsync(true);
+
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"ConfirmUser error:\n{ex}");
+            return Results.InternalServerError();
+        }
+    });
+
+
+
 
 app.MapGet("/", () => "waguri says hello!");
 // /sign-up
@@ -127,7 +185,7 @@ app.MapPost(
         // send confirmation email
         var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
         var code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
-        var callbackUrl = $"https://localhost:7140/sign-up/confirm/{user.Id}/{code}";
+        var callbackUrl = $"http://localhost:5000/sign-up/confirm/{user.Id}/{code}";
         var appPassword = Environment.GetEnvironmentVariable("EMAIL_APP_PASSWORD");
         var senderMail = "douglasaubre@gmail.com";
         var senderName = "douglas aubre";
